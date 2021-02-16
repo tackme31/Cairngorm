dafe6b4b448f74f24fb5967cd64f570e39f87227
Takumi Yamada <xirtardauq@gmail.com>
2021-02-16 16:32:42 +0900

    Use the Request.Cookies when user accesses a non-target page.

--- Cairngorm/ItemsStores/CookieItemsStore.cs
+++ Cairngorm/ItemsStores/CookieItemsStore.cs
@@ -29,14 +29,17 @@ namespace Cairngorm.ItemsStores
 
         public override List<Item> GetItems()
         {
-            var cookieValue = HttpContext.Current.Response.Cookies[CookieInfo.Name]?.Value ?? string.Empty;
-            return cookieValue.Split('|')
-                .Select(idStr => ID.Parse(idStr, ID.Null))
-                .Where(id => !ID.IsNullOrEmpty(id))
-                .Select(Context.Database.GetItem)
-                .Where(item => item != null)
-                .Reverse()
-                .ToList();
+            var cookie = HttpContext.Current.Response.Cookies.AllKeys.Contains(CookieInfo.Name)
+                ? HttpContext.Current.Response.Cookies[CookieInfo.Name]
+                : HttpContext.Current.Request.Cookies[CookieInfo.Name];
+            if (cookie == null || string.IsNullOrWhiteSpace(cookie.Value))
+            {
+                return new List<Item>();
+            }
+
+            var ids = cookie.Value.Split('|').Select(idStr => ID.Parse(idStr, ID.Null));
+            var items = ids.Select(Context.Database.GetItem).Where(item => item != null);
+            return items.Reverse().Take(10).ToList();
         }
 
         protected virtual HttpCookie GetCookie()
