ed4c724b3fd91e61084616495bf0d1bd2a4a7e5d
Takumi Yamada <xirtardauq@gmail.com>
2021-09-28 02:00:12 +0900

    Add a computed field for indexing tags configured in tag resolvers.

---
+++ Cairngorm/ContentSearch/RecommenderTagResolversIndexField.cs
@@ -0,0 +1,54 @@
+﻿using Cairngorm.Configurations;
+using Sitecore.ContentSearch;
+using Sitecore.ContentSearch.ComputedFields;
+using Sitecore.Diagnostics;
+using Sitecore.Xml;
+using System.Linq;
+using System.Xml;
+
+namespace Cairngorm.ContentSearch
+{
+    public class RecommenderTagResolversIndexField : AbstractComputedIndexField
+    {
+        protected string RecommenderName { get; }
+
+        protected RecommenderTagResolversIndexField() : base()
+        {
+        }
+
+        protected RecommenderTagResolversIndexField(XmlNode configNode) : base(configNode)
+        {
+            RecommenderName = XmlUtil.GetAttribute("recommender", configNode);
+        }
+
+        public override object ComputeFieldValue(IIndexable indexable)
+        {
+            if (!(indexable is SitecoreIndexableItem indexableItem))
+            {
+                return null;
+            }
+
+            var item = indexableItem.Item;
+            if (item == null)
+            {
+                return null;
+            }
+
+            var config = RecommenderConfiguration.Create();
+            if (config == null)
+            {
+                return null;
+            }
+
+            var setting = config.GetSetting(RecommenderName);
+            if (setting == null)
+            {
+                Log.Warn($"The specified recommender '{RecommenderName}' does not exist.", this);
+                return null;
+            }
+
+            var tags = setting.TagResolvers.SelectMany(resolver => resolver.GetItemTags(item));
+            return string.Join(" ", tags);
+        }
+    }
+}
